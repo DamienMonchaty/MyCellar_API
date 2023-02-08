0e87d2368efbcae5bdae46573e5287e725fbb42a
DamienMonchaty <damienmonchaty@hotmail.fr>
2023-02-08 15:03:54 +0100

    1.1 Mise en place, créeation & configuration projet

---
+++ MyCellar.Business/Context/ModelDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace MyCellar.Business.Context
+{
+    public class ModelDbContext : DbContext
+    {
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connetionString = "server=localhost:3307;database=mybase;user=root;password=root";
+            optionsBuilder.UseMySql(connetionString, ServerVersion.AutoDetect(connetionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
